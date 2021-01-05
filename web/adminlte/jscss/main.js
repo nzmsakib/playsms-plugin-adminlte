d92cf01ed93a5e6e93e420a972c7095cb1620066
Anton Raharja <antonrd@gmail.com>
2021-01-05 12:23:21 +0700

    update main.css following playSMS 1.4.4 beta3, fix page-header

--- web/adminlte/jscss/main.js
+++ web/adminlte/jscss/main.js
@@ -20,22 +20,24 @@ $(document).ready(function() {
 	$("input[type='text']").addClass("form-control playsms-input-sizer");
 	$("input[type='password']").addClass("form-control playsms-input-sizer");
 	$("input[type='file']").addClass("form-control playsms-input-sizer");
-	$("input[type='hidden']").addClass("playsms-input-sizer");
 	
 	$("input[type='text']").css("display", "inline");
 	$("input[type='password']").css("display", "inline");
 	$("input[type='file']").css("display", "inline");
-	$("input[type='hidden']").css("display", "inline");
 
 	$("textarea").addClass("form-control playsms-input-sizer");
 	
 	$("select").addClass("form-control playsms-input-sizer");
 	
 	var page_header_title = $(".page-header-title").text();
-	$(".this-page-header-title").text(page_header_title);
-	$(".page-header-title").remove();
+	if (!page_header_title.trim()) {
+		$(".this-page-header-title").text(page_header_title);
+		$(".page-header-title").remove();
+	}
 
 	var page_header_subtitle = $(".page-header-subtitle").text();
-	$(".this-page-header-subtitle").text(page_header_subtitle);
-	$(".page-header-subtitle").remove();
+	if (!page_header_subtitle.trim()) {
+		$(".this-page-header-subtitle").text(page_header_subtitle);
+		$(".page-header-subtitle").remove();
+	}
 });
